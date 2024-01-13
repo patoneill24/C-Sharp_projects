babe2b2d387499292f6c6b7e025028febd2f83e6
patoneill24 <poneill33@byui.edu>
2024-01-12 19:35:16 -0700

    Completed the Core challenges for the conditional activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeLetter = "";
+        string userInput = Console.ReadLine();
+        int gradeNumber = int.Parse(userInput);
+        if (gradeNumber >= 90) 
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else{
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is a(n) {gradeLetter}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class, better luck next time!");
+        }
     }
 }
